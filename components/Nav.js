986d609ba01ebb1226cf63ecd6f0dd37f0128d78
Rupen Bhimani <rupenbhimani109@gmail.com>
2022-11-28 00:30:48 +0530

    update few things

--- components/Nav.js
+++ components/Nav.js
@@ -5,7 +5,6 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 export default function Nav() {
 
     const [user, loading, error] = useAuthState(auth);
-    console.log(user);
 
     return (
         <nav className="flex justify-between items-center py-10">
