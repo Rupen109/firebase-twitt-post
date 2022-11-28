91b4d19e22305ae94967fa062c2f94894c9cf257
Rupen Bhimani <rupenbhimani109@gmail.com>
2022-11-28 16:49:15 +0530

    changes on firebase

--- utils/firebase.js
+++ utils/firebase.js
@@ -3,7 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 
-// Your web app's Firebase configuration
+
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_API_KEY,
     authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
