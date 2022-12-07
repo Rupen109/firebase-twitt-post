b0d189d5207f685cc3b7159d304f1ed91934e76d
Rupen Bhimani <rupenbhimani109@gmail.com>
2022-12-07 17:43:33 +0530

    improve ui

--- components/SideNavbar.js
+++ components/SideNavbar.js
@@ -37,9 +37,9 @@ function SideNavbar() {
         <div className="">
             {user && (   
                 <Disclosure as="nav">
-                <Disclosure.Button className="absolute top-4 right-4 inline-flex items-center peer justify-center rounded-md p-2 text-gray-800 hover:bg-gray-900 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white group">
+                <Disclosure.Button className="absolute top-4 right-4 inline-flex items-center peer justify-center rounded-md p-2 text-gray-800 hover:bg-gray-900 hover:text-white focus:outline-none ">
                     <GiHamburgerMenu
-                        className="block md:hidden h-6 w-6 text-white mt-14"
+                        className="block md:hidden h-6 w-6 text-white -mt-4 "
                         aria-hidden="true"
                     />
                 </Disclosure.Button>
